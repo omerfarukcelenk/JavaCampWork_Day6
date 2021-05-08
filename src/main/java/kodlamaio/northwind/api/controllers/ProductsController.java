4e12ab55d09d2700a64071a1c8d61c0ef4874b1b
siltri <omerfar0133@gmail.com>
2021-05-09 01:24:59 +0300

    first commit

---
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,31 @@
+package kodlamaio.northwind.api.controllers;
+
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+	
+	private ProductService productService;
+	
+	
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+
+	@GetMapping("/getall")
+	public List<Product> getAll(){
+		return this.productService.getAll();
+	}
+}
+
